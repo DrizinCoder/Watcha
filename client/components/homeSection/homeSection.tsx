d72494de5eb3e736369f9f08e9e89c847f3156e3
Guilherme Fernandes <guiaulas2020@gmail.com>
2026-02-07 00:45:06 -0300

    feat: update homeSection to consume api

--- client/components/homeSection/homeSection.tsx
+++ client/components/homeSection/homeSection.tsx
@@ -1,19 +1,27 @@
 import Link from "next/link"
 import { VideoCard } from "../ui/videoCard/videoCard"
+import { useEffect, useState } from "react"
+import { videoService } from "@/services/api"
 
-const videos = [
-    { id: '1', title: 'Aventura nas Montanhas', thumb: 'https://picsum.photos/seed/1/400/225' },
-    { id: '2', title: 'Review Tecnológico', thumb: 'https://picsum.photos/seed/2/400/225' },
-    { id: '3', title: 'Culinária 5 Estrelas', thumb: 'https://picsum.photos/seed/3/400/225' },
-  ]
+interface VideoProps {
+  id: string;
+  title: string;
+  thumb: string;
+}
 
 export const HomeSection = () => {
+    const [videos, setVideos] = useState([])
+
+    useEffect(() => {
+      videoService.getAll().then(data => setVideos(data)).catch(err => console.error("Back-end ainda tá off", err))
+    }, [])
+
     return (
     <div className="p-8">
         <h2 className="text-xl font-semibold mb-6">Recomendados para você</h2>
         
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          {videos.map(video => (
+          {videos.map((video:VideoProps)  => (
             <Link href={`/watch/${video.id}`} key={video.id}>
               <VideoCard 
                 title={video.title}
